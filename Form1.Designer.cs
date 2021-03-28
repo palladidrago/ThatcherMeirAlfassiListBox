c93280b476f665e50ba897fdffaef3f61b74dfb1
Max Power <palladidrago@gmail.com>
2021-03-29 00:53:06 +0300

    First, and hopefully last, commit(Praise Roni)

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace ThatcherSongListBox___Ashi_Alfassi__Meir_Di_Capua
+{
+    partial class SongListBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lyricBox = new System.Windows.Forms.ListBox();
+            this.iCarlyButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lyricBox
+            // 
+            this.lyricBox.Font = new System.Drawing.Font("Rockwell Nova Cond", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lyricBox.FormattingEnabled = true;
+            this.lyricBox.ItemHeight = 44;
+            this.lyricBox.Location = new System.Drawing.Point(12, 158);
+            this.lyricBox.Name = "lyricBox";
+            this.lyricBox.Size = new System.Drawing.Size(688, 136);
+            this.lyricBox.TabIndex = 0;
+            // 
+            // iCarlyButton
+            // 
+            this.iCarlyButton.BackColor = System.Drawing.Color.Transparent;
+            this.iCarlyButton.Font = new System.Drawing.Font("Georgia Pro Cond", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iCarlyButton.Location = new System.Drawing.Point(502, 28);
+            this.iCarlyButton.Name = "iCarlyButton";
+            this.iCarlyButton.Size = new System.Drawing.Size(198, 91);
+            this.iCarlyButton.TabIndex = 1;
+            this.iCarlyButton.Text = "iCarly";
+            this.iCarlyButton.UseVisualStyleBackColor = false;
+            this.iCarlyButton.Click += new System.EventHandler(this.SongPlay);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(439, 140);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Explanation: Click the button to play iCarly\'s intro, and the lyrics will be show" +
+    "n in the listbox";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // SongListBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(719, 616);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.iCarlyButton);
+            this.Controls.Add(this.lyricBox);
+            this.Name = "SongListBox";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lyricBox;
+        private System.Windows.Forms.Button iCarlyButton;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Media;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ThatcherSongListBox___Ashi_Alfassi__Meir_Di_Capua
+{
+    public partial class SongListBox : Form
+    {
+        public SongListBox()
+        {
+            InitializeComponent();
+            player = new SoundPlayer();
+        }
+        SoundPlayer player;
+
+        private int HeightSum()
+        {
+            return lyricBox.ItemHeight * lyricBox.Items.Count;
+        }
+
+        private async void SongPlay(object sender, EventArgs e)
+        {
+            //Plays a song and shows its' lyrics.
+            //Take song from button text.
+
+            string song = (sender as Button).Text.Replace(" ","");
+            
+            
+            //Start player.
+            Stream s = Properties.Resources.ResourceManager.GetObject(song) as Stream;
+            player.Stream = s;
+            player.Play();
+
+            //Disable button.
+            ((Button)sender).Enabled = false;
+
+            //Save bottom and item height for later use.
+            int formHeight = this.Height;
+            int lyricBoxItemHeight = lyricBox.ItemHeight;
+
+            //Load song lyrics.
+            string songLyrics = Properties.Resources.ResourceManager.GetObject(song + "Lyrics") as String;
+            string[] songLines = System.Text.RegularExpressions.Regex.Split(songLyrics, Environment.NewLine);
+
+            //Starting time, and vars.
+            int startTime = int.Parse(songLines[0]);
+            await Task.Delay(startTime);
+            int prevTime = startTime, curTime;
+
+            //Lyric showing
+            for (int i = 1; i < songLines.Length; i += 2)
+            {
+                lyricBox.Items.Add(songLines[i]);
+                lyricBox.SelectedIndex = lyricBox.Items.Count - 1;
+                if (HeightSum() > lyricBox.Height && lyricBox.Bottom < formHeight - lyricBoxItemHeight * 2)
+                {
+                    lyricBox.Height += lyricBoxItemHeight;
+                }
+                curTime = int.Parse(songLines[i + 1]);
+                await Task.Delay(curTime - prevTime);
+                prevTime = curTime;
+            }
+            ((Button)sender).Enabled = false;
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "Explanation: Click the button to play iCarly's intro, and the lyrics will be shown in the listbox";
+        }
+    }
+}
